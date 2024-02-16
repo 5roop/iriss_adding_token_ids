113d545623a710ab7f1a184817ba7c0723d9a102
Peter Rupnik <prupnik@e8-login.ijs.si>
2024-02-16 09:55:08 +0100

    Add input downloading script

---
+++ 000_download_data.sh
@@ -0,0 +1,8 @@
+wget https://github.com/5roop/iriss_debug_and_salvage/raw/main/Iriss-disfl-anno-phase1.zip
+wget https://github.com/5roop/iriss_debug_and_salvage/raw/main/iriss_with_w_and_pauses.zip
+
+yes | unzip Iriss-disfl-anno-phase1.zip
+yes | unzip iriss_with_w_and_pauses.zip
+
+rm Iriss-disfl-anno-phase1.zip
+rm iriss_with_w_and_pauses.zip
